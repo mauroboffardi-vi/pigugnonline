9b05de9b74be1564f5d9b3c5f7ded4e53c62c76f
Mauro Boffardi <mauro.boffardi@vincit.fi>
2026-09-02 18:19:17 +0200

    Corretta la build di Vite per rispettare il path locale ./ invece che /

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 // Autogenerato da scripts/generate-version.js - NON MODIFICARE MANUALE
 export const APP_VERSION = {
-  "version": "5953407-dirty",
-  "buildTime": "2026-09-02T16:08:59.310Z"
+  "version": "f8fe1ff-dirty",
+  "buildTime": "2026-09-02T16:16:44.110Z"
 } as const;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
     // Indica a Vite che i sorgenti sono nella cartella src
     root: 'src',
+
+    base: './', // <-- Questo trasforma gli URL da "/assets/..." a "./assets/..."
+
     build: {
         // L'output della build andrà nella cartella dist (nella root del progetto)
         outDir: '../dist',
@@ -12,6 +19,7 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'src/index.html'),
                 single: resolve(__dirname, 'src/app/single/single.html'),
+                // multiplayer: resolve(__dirname, 'src/app/multiplayer/multiplayer.html'),            
             }
         }
     }
