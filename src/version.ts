7ffbbdeeb6d29acef7bba728aea04ebd999e818d
Mauro Boffardi <mauro.boffardi@vincit.fi>
2026-09-04 14:51:48 +0200

    Aggiunti alcuni banter

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 // Autogenerato da scripts/generate-version.js - NON MODIFICARE MANUALE
 export const APP_VERSION = {
-  "version": "e7dc1f6-dirty",
-  "buildTime": "2026-09-04T12:27:30.194Z"
+  "version": "6e1ff8b",
+  "buildTime": "2026-09-04T12:43:39.522Z"
 } as const;
