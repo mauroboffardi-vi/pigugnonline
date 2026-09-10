1248de4747bb65ea9565fcaec6420663413a13e2
Mauro Boffardi <mauro.boffardi@vincit.fi>
2026-09-10 15:14:33 +0200

    aggiustate dimensioni fumetto per versione mobile

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 // Autogenerato da scripts/generate-version.js - NON MODIFICARE MANUALE
 export const APP_VERSION = {
-  "version": "95a678d-dirty",
-  "buildTime": "2026-09-10T13:01:43.705Z"
+  "version": "54d3a4e-dirty",
+  "buildTime": "2026-09-10T13:13:17.924Z"
 } as const;
